541e3898a6b4f8df62590b2804fccdbf269d8924
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-04-24 16:48:50 -0400

    Day 21 complete

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Chris Wu on 4/22/20.
+//  Copyright © 2020 Chris Wu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    var message = ""
+    var body: some View {
+        ZStack {
+            //Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of").foregroundColor(.white)
+                    Text("\(countries[correctAnswer])")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                ForEach (0..<3) { number in
+                    Button(action: {
+                        self.evaluateFlag(myAnswer: number)
+                    }) {
+                        Image(self.countries[number]).renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+    
+    func evaluateFlag(myAnswer : Int) {
+        if myAnswer == correctAnswer {
+            scoreTitle = "You got it right"
+        } else {
+            scoreTitle = "You got it wrong!"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
